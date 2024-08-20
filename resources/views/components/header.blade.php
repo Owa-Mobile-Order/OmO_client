503a3064f6704e22ddfa5ba77ed20fd7ec511e14
Harukotokoto <n_rui@outlook.jp>
2024-08-21 02:18:39 +0900

    パスを変更

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -6,21 +6,21 @@
     </div>
     <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div class="text-sm lg:flex-grow">
-            <a href="#responsive-header" class="block text-xl mt-4 lg:inline-block lg:mt-0 mr-4 hover:underline">
+            <a href="/order" class="block text-xl mt-4 lg:inline-block lg:mt-0 mr-4 hover:underline">
                 予約
             </a>
-            <a href="#responsive-header" class="block text-xl mt-4 lg:inline-block lg:mt-0 mr-4 hover:underline">
+            <a href="/terms" class="block text-xl mt-4 lg:inline-block lg:mt-0 mr-4 hover:underline">
                 利用規約
             </a>
-            <a href="#responsive-header" class="block text-xl mt-4 lg:inline-block lg:mt-0 hover:underline">
+            <a href="/privacy" class="block text-xl mt-4 lg:inline-block lg:mt-0 hover:underline">
                 プライバシーポリシー
             </a>
         </div>
 
         <div
-            class="my-auto flex h-10 w-28 rounded-md border border-teal-500 text-black hover:bg-teal-500 hover:text-white transition my-4 lg:w-32">
-            <a href="#" class="flex items-center w-full h-full justify-center">Login</a>
+            class="flex h-10 w-28 rounded-md border border-gray-400 text-black hover:bg-gray-100 transition my-4 lg:w-32">
+            <a href="/login" class="flex items-center w-full h-full justify-center">Login</a>
         </div>
     </div>
 </nav>
-<hr class="border-gray-200 mx-8">
+<hr class="border-gray-200 mx-4">
