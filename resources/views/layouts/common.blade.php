d9b8b7809c59f062435cde979ab8e63de7e94ba0
Harukotokoto <n_rui@outlook.jp>
2024-08-25 10:23:02 +0900

    記述方法を修正

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -1,17 +1,23 @@
-<head>
-  <title>@yield('title')</title>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <title>@yield('title')</title>
 
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
 
-  @yield('head')
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-  @vite('resources/css/app.css')
-</head>
-<body>
-  @include('components.header')
-  <div class="pt-20">
-    @yield('body')
-  </div>
-</body>
+    @yield('head')
+
+    @vite('resources/css/app.css')
+  </head>
+  <body>
+    @include('components.header')
+    <div class="pt-20">
+      @yield('body')
+    </div>
+  </body>
+</html>
