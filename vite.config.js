8216f715e0150ff85b0a284db11ec5263fa92c27
Harukotokoto <y.games@outlook.jp>
2024-08-26 22:47:36 +0900

    HTTPSの設定を追記

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 dotenv.config();
 
 export default defineConfig({
