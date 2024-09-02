78953258c34d0834cb866c116b2981dc17da7e99
Harukotokoto <n_rui@outlook.jp>
2024-09-02 19:34:20 +0900

    しーだーを削除

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,16 +14,5 @@ class DatabaseSeeder extends Seeder
    */
   public function run(): void
   {
-    DB::table('users')->insert([
-      'email' => 'test@example.com',
-      'password' => Hash::make('test'),
-      'remember_token' => null,
-      'name' => 'Test User',
-      'avatar_path' =>
-        'https://cdn.discordapp.com/attachments/1149350818747781122/1277555246650097674/quote.jpg?ex=66cd978a&is=66cc460a&hm=5578f04344e356d80dedc71c8aa9f061b465329c1074f7f6852615deab00426a&',
-      'student_code' => 'TESTUSER',
-      'created_at' => now(),
-      'updated_at' => now(),
-    ]);
   }
 }
